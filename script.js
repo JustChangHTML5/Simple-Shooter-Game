55df59d0029e1f877eed03f3f52d34c89fa874f9
Ivan Chang <>
2021-06-08 04:17:58 +0000

    Bug fixes, Color scheme change, Bullet class, more organized

--- script.js
+++ script.js
@@ -1,13 +1,31 @@
 var canvas = document.getElementById("canvas");//Canvas
 var ctx = canvas.getContext("2d");//Context
 
-canvas.width = 100;//Set Canvas size
-canvas.height = 100;//Set Canvas size
+var width = 1000;
+var height = 600;
+
+canvas.width = width;//Set Canvas size
+canvas.height = height;//Set Canvas size
+
+class Bullet {
+    constructor(posX, posY, width, height, velocityX, velocityY, strength) {
+        this.pos = [posX, posY];//Bullet position
+        this.velocity = [velocityX, velocityY];
+        this.size = [width, length];//Size
+        this.strength = strength; //Bullet attack strength
+    }
+
+    move() {
+        this.pos[0] += this.velocity[0];
+        this.pos[1] += this.velocity[1];
+    }
+
+}
 
 class Shooter {
     constructor(posX, posY, width, height, hp, strength) {
-        this.pos = #[posX, posY];//Shooter position
-        this.size = #[width, length];//Size
+        this.pos = [posX, posY];//Shooter position
+        this.size = [width, length];//Size
         this.hp = hp;//Shooter hp
         this.strength = strength;//Shooter attack strength
     }
