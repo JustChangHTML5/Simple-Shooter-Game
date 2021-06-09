f0022c62d0b00de25f8b61a42e0d9fc04444c3a5
Ivan Chang <80505615+JustChangHTML5@users.noreply.github.com>
2021-06-09 05:12:10 +0000

    Aim with big ol formula

--- script.js
+++ script.js
@@ -7,6 +7,9 @@ var height = 600;
 canvas.width = width;//Set Canvas size
 canvas.height = height;//Set Canvas size
 
+var bullets = [];//bullets list
+var shooters = [];//enemies list
+
 class Bullet {
     constructor(posX, posY, width, height, velocityX, velocityY, strength) {
         this.pos = [posX, posY];//Bullet position
@@ -16,15 +19,20 @@ class Bullet {
     }
 
     move() {
-        this.pos[0] += this.velocity[0];
-        this.pos[1] += this.velocity[1];
+        if (this.pos[0] < 0 || this.pos[0] > width || this.pos[1] < 0 || this.pos[1] > height) {
+            this = null;//SELF DESTRUCT
+        }
+        this.pos[0] += this.velocity[0];//moveX
+        this.pos[1] += this.velocity[1];//moveY
     }
 
     checkIfHit(shooter) {
         if (this.pos[0] > shooter.pos[0] && this.pos[0] < (shooter.pos[0] + shooter.size[0]) && this.pos[1] > shooter.pos[1] && this.pos[1] < (shooter.pos[1] + shooter.size[1])) {
-            shooter.hp -= this.damage;
+            shooter.hp -= this.damage;//take damage when hit
         }
     }
+
+    //If out of bounds, despawn
 
 }
 
@@ -34,10 +42,12 @@ class Shooter {
         this.size = [width, length];//Size
         this.hp = hp;//Shooter hp
         this.strength = strength;//Shooter attack strength
+        this.direction = 0;//Direction
     }
 
     aim(aimX, aimY) {
         //Looks at a certain position
+        this.direction = (180 - Math.asin(Math.sqrt(Math.pow(Math.abs(aimX - this.pos[0]), 2) + Math.pow(Math.abs(aimY - this.pos[1], 2)) / abs(y1 - y0))) * Math.PI / 180; //big ol pointer
     }
 
     shoot(shootX, shootY) {
